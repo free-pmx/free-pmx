951f9d45c525a4da61d711c230b5311d7dc1e8c0
free-pmx <free-pmx>
2024-12-13 17:18:39 +0000

    code cleanup + documentation

--- snippets/pve-no-nag.pl
+++ snippets/pve-no-nag.pl
@@ -1,28 +1,49 @@
 #!/usr/bin/perl -pi.bak
 
+# NAME
+#        pve-no-nag.pl - Proxmox VE no-subscription popup removal tool
 #
-# Proxmox VE no-subscription popup removal tool
+# SYNOPSIS
+#        pve-no-nag.pl [FILE]...
 #
-# Maintained at: https://github.com/free-pmx/
+# DESCRIPTION
+#        Searches and replaces entire offending piece of code in-place.
 #
-# Usage:
+#        With no FILE, or when FILE is -, read standard input.
 #
-#   perl pve-no-nag.pl /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
-# 
-# Check:
+# USAGE, VERIFICATION AND REVERSION
+#        perl pve-no-nag.pl /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 #
-#   diff -u /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js{.bak,}
+#               Applies the patch on the library at its usual installation path.
+#               Requires no execute permission on the script.
 #
-# Revert:
+#        diff -u /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js{.bak,}
 #
-#   apt reinstall proxmox-widget-toolkit
+#               Compares the applied changes against the retained copy of the original.
+#
+#        apt reinstall proxmox-widget-toolkit
+#
+#               Reverts the modification by reinstalling the original component.
+#
+# NOTES
+#
+#        Always creates a backup copy of the original into a .bak file.
+#
+# BUGS
+#
+#        Bugs can be reported at free-pmx GitHub repository <https://github.com/free-pmx/>.
+#
+# LICENSE
+#        License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+#        This is free software: you are free to change and redistribute it.
+#        There is NO WARRANTY, to the extent permitted by law.
 #
 
 use strict;
 use warnings;
 
-
-my $_PATTERN_ = << 'EOF';
+# original
+my $o = quotemeta << 'EOF';
     checked_command: function(orig_cmd) {
 	Proxmox.Utils.API2Request(
 	    {
@@ -56,7 +77,8 @@ my $_PATTERN_ = << 'EOF';
     },
 EOF
 
-my $_SUBSTITUTE_ = << 'EOF';
+# replacement
+my $r = << 'EOF';
     checked_command: function(orig_cmd) {
 	Proxmox.Utils.API2Request(
 	    {
@@ -73,7 +95,4 @@ my $_SUBSTITUTE_ = << 'EOF';
     },
 EOF
 
-
-BEGIN {undef $/;}
-
-s/\Q$_PATTERN_\E/$_SUBSTITUTE_/;
+BEGIN { undef $/; } s/$o/$r/;
